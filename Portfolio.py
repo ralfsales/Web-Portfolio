46b6480e5ba7b86e7a7f7b838a41e95e3d7fb4fb
Rafael <77760494+ralfsales@users.noreply.github.com>
2025-02-06 18:11:57 +0000

    Update on database file

--- Portfolio.py
+++ Portfolio.py
@@ -96,14 +96,14 @@ col3, col4 = st.columns(2)
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
-    for index, row in df[:10].iterrows():
+    for index, row in df[:5].iterrows():
         st.header(row["title"])
         st.image("images/" + row["image"])
         st.write(row["description"])
         st.write(f"[Source Code]({row['url']})")
 
 with col4:
-    for index, row in df[10:].iterrows():
+    for index, row in df[6:].iterrows():
         st.header(row["title"])
         st.image("images/" + row["image"])
         st.write(row["description"])
